dd0adc7cf71f266dd23741d942cf5f6af3a91178
Winfried Koenig <w.koenig@acm.org>
2003-03-28 16:34:04 +0000

    Import of WINKO/String-BitCount-1.12 from CPAN.
    gitpan-cpan-distribution: String-BitCount
    gitpan-cpan-version:      1.12
    gitpan-cpan-path:         WINKO/String-BitCount-1.12.tar.gz
    gitpan-cpan-author:       WINKO
    gitpan-cpan-maturity:     released

--- BitCount.pm
+++ BitCount.pm
@@ -1,17 +1,19 @@
+# $Id: BitCount.pm,v 1.4 2003/03/28 16:21:38 win Exp $
+
 package String::BitCount;
 
+use 5.004;
 use strict;
+use Carp 'carp';
 use vars qw(
-    $VERSION $Version $Revision
-    @ISA @EXPORT
+    $VERSION @ISA @EXPORT
 );
 
 require Exporter;
 
 @ISA = qw(Exporter);
 
-$Version = $VERSION = "1.11";
-($Revision = substr(q$Revision: 1.2 $, 10)) =~ s/\s+$//;
+$VERSION = "1.12";
 
 @EXPORT = qw(BitCount showBitCount);
 
@@ -20,9 +22,25 @@ for (0 .. 7) {
     $bits .= join '', map { ++$_ } split '', $bits;
 }
 
+my $test = '';
+eval "require 5.006";
+if (!$@) {
+    eval "require 5.008";
+    if ($@) {
+	# Perl v5.6.0 or v5.6.1
+	$test = 'do { use bytes; length }  != do { no bytes; length }';
+    } else {
+	# Perl v5.8.0 and later
+	$test = 'tr/\0-\377/\0/c';
+    }
+    $test .= ' && carp "Wide character in argument";';
+}
+
 eval <<EDQ;
     sub BitCount {
-	(local \$_ = join '', \@_) =~ tr/\\0-\\377/$bits/;
+	# can't use join, wide char bug in v5.6
+	local \$_ = shift; while (\@_) { \$_ .= shift }
+	$test \$_ =~ tr/\\0-\\377/$bits/;
 	tr/1// + tr/2// * 2 + tr/3// * 3 + tr/4// * 4
 	       + tr/5// * 5 + tr/6// * 6 + tr/7// * 7 + tr/8// * 8;
     }
@@ -30,7 +48,7 @@ eval <<EDQ;
     sub showBitCount {
 	my(\@s) = \@_;
 	foreach (\@s) {
-	    tr/\\0-\\377/$bits/;
+	    $test tr/\\0-\\377/$bits/;
 	}
 	wantarray ? \@s : join '', \@s;
     }
@@ -38,16 +56,25 @@ EDQ
 die $@ if $@;
 
 1;
-
 __END__
 
 =head1 NAME
 
-String::BitCount, BitCount showBitCount - count number of "1" bits in string
+String::BitCount - count number of "1" bits in strings
 
 =head1 SYNOPSIS
 
-    use String::BitCount;
+  use String::BitCount;
+
+  # next line recommended for Perl versions 5.006 and newer.
+  use bytes;
+
+  # get the number of bits in $string.
+  my $count = BitCount($string);
+
+  # prints '334'; The number of bits in the codes
+  # of 'a', 'b' and 'c' are '3', '3' and '4'.
+  print showBitCount('abc'), "\n";
 
 =head1 DESCRIPTION
 
@@ -57,6 +84,7 @@ String::BitCount, BitCount showBitCount - count number of "1" bits in string
 
 Joins the elements of LIST into a single string
 and returns the the number of bits in this string.
+Only code points in the range 0x00 .. 0xFF are allowed.
 
 =item showBitCount LIST
 
@@ -68,9 +96,16 @@ new list into a single string and returns the string.
 
 =back
 
+=head1 NOTES
+
+The arguments of this subroutines are restricted to strings of 
+characters having code points in the range 0x00 .. 0xFF.
+If any string argument has code points greater than 0xFF (255)
+a "Wide character" warning will be issued.
+
 =head1 AUTHOR
 
-Winfried Koenig <win@in.rhein-main.de>
+Winfried Koenig <w.koenig@acm.org>
 
 =head1 SEE ALSO
 
